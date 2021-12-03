ee4e95739603799ccb385cd9e73258dda143411d
kroppel <konsti.roppel@t-online.de>
2021-12-03 09:51:35 +0100

    Added 3 solutions for first puzzle

---
+++ day-1/day-1.py
@@ -0,0 +1,38 @@
+import numpy as np
+import time
+import itertools
+
+def solution0(input):
+	for num1 in input:
+		for num2 in input:
+			if ((num1 + num2) == 2020 and num1 != num2):
+				print(str(num1) + ', ' + str(num2) + ', ' + str(num1 * num2))
+				break
+		else:
+			continue
+		break
+
+def solution1(input):
+	for num1 in input:
+		remainder = 2020 - num1
+		if (num1 != remainder):
+			if (np.isin(input, [remainder], True).sum() > 0):
+				print(str(num1) + ', ' + str(remainder) + ', ' + str(num1*remainder))
+				break
+
+def solution2(input):
+	for (num1, num2) in itertools.product(input, input):
+		if ((num1 + num2) == 2020 and num1 != num2):
+				print(str(num1) + ', ' + str(num2) + ', ' + str(num1 * num2))
+				break
+
+input = np.array(open('input.txt').read().split('\n')[:-1], int)
+solutions = [solution0, solution1, solution2]
+exec_times = []
+
+for i in np.arange(3):
+	start_time = time.time()
+	solutions[i](input)
+	exec_times.append(time.time() - start_time)
+
+print(exec_times)
